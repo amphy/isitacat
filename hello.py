cda21b37f2d7054b3c5fa0844554575964119d97
Lord and Master of All Things Big and Small <amphy.kit@gmail.com>
2016-02-08 13:59:29 -0500

    attempt http get to clarifai api

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
 import os
+import json
+import requests
 from flask import Flask
 from flask import render_template
 from flask import request
+#from query import Query
 
 app = Flask(__name__)
+
+url = "https://api.clarifai.com/v1/tag/?url="
+headers = {"Authorization" : "Bearer VwtBF0lLJQ3UkGvVoyOeWCX0G4GCCa"}
 
 @app.route("/")
 def hello():
@@ -12,6 +18,8 @@ def hello():
 @app.route("/result", methods=["GET", "POST"])
 def checkCat():
     img = request.form['imgurl']
+    url = url + img
+    r = requests.get(url, headers=headers)
     return render_template('result.html', imgurl = img)
 
 if __name__ == "__main__":
