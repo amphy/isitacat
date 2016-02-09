197372fc38602537b0644c32fe213029d3157df6
Lord and Master of All Things Big and Small <amphy.kit@gmail.com>
2016-02-08 21:00:03 -0500

    more restructuring

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 #from query import Query
-from client import ClarifaiApi
+from clarifai2.client import ClarifaiApi
 
 app = Flask(__name__)
 
