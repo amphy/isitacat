06c75f7b3c914ab67615156a240eeaf63617e4a9
Lord and Master of All Things Big and Small <amphy.kit@gmail.com>
2016-02-08 21:31:14 -0500

    getting json working

--- hello.py
+++ hello.py
@@ -24,7 +24,7 @@ def checkCat():
     result = clarifai_api.tag_image_urls(img)
     res =  json.dumps(result)
     parsed_json = json.loads(res)
-    imgtag = result['result']['tag']['classes']
+    imgtag = res['result']['tag']['classes']
     print imgtag
     #for x in imgtag:
         #tagging.append(str(x))
